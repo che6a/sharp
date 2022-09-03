efa6eeebe6d7468e4fb3e85b9bb0f47129d2854d
d0gge <ejs.defolt@gmail.com>
2022-09-03 18:55:47 +0500

    feat: done 0 lab

---
+++ lab0/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace d0gge
+{
+    class Program
+    {
+        static void Main()
+        {
+            DateTime now = DateTime.Now;
+            Console.WriteLine($"{now}: Hello, World!");
+        }
+    }
+
+}
