41f53c1d87dd13033344b910def00acdabba3683
Egor <ejs.defolt@gmail.com>
2022-09-05 22:39:48 +0500

    feat: implemented all requirements

--- lab3/Program.cs
+++ lab3/Program.cs
@@ -1,116 +1,56 @@
 ﻿using System;
 
+
 namespace d0gge
 {
-    abstract class Plane : IComparable<Plane>
-    {
-
-        public Plane() {}
-        public Plane(string name, int race, double mass, int seatNumber, string[] crew)
-        {
-            _mass = mass;
-            _seatNumber = seatNumber;
-            _crew = crew;
-            _name = name;
-            _race = race;
-        }
-        protected abstract double CalculateTakeOffMass();
-        public int CompareTo(Plane other)
-        {
-            if(!Convert.ToBoolean(TakeOffMass.CompareTo(other.TakeOffMass)))
-            {
-                return Race.CompareTo(other.Race);
-            }
-            else
-            {
-                return TakeOffMass.CompareTo(other.TakeOffMass);
-            }
-        }
-        protected int Race { get { return _race; } private set { _race = value; }}
-        protected string? Name { get { return _name; } private set { _name = value; }}
-        protected double Mass { get { return _mass; } private set { _mass = value; }}
-        protected double TakeOffMass { get { return _takeOffMass; } set { _takeOffMass = value; }}
-        protected string? Type { get { return _type; } set { _type = value; } }
-        protected string[]? Crew {get { return _crew; } private set { _crew = value; }}
-        protected int SeatNumber { get { return _seatNumber; } private set { _seatNumber = value; }}
-        public override string ToString() => $"Type: {Type}. {Name} "
-            + $"race #{Race} take off mass: {TakeOffMass}";
-
-        protected double _mass;
-        protected double _takeOffMass;
-        protected string? _type;
-        protected string[]? _crew;
-        protected int _seatNumber;
-        protected string? _name;
-        protected int _race;
-    }
-
-    class PassengerPlane : Plane
-    {
-        private const double AveragePersonMass = 62;
-
-        public PassengerPlane(string name, int race, double mass, int seatNumber, string[] crew)
-            : base(name, race, mass, seatNumber, crew)
-        { 
-            _type = "PASSENGER";
-            _takeOffMass = CalculateTakeOffMass();
-        }
-
-        protected override double CalculateTakeOffMass()
-            => SeatNumber * AveragePersonMass;
-    }
-
-    class CargoPlane : Plane
-    {
-        public CargoPlane(string name, int race, double mass, string[] crew)
-            : base(name, race, mass, 0, crew)
-        {
-            _type = "CARGO";
-            _takeOffMass = CalculateTakeOffMass();
-        }
-
-        protected override double CalculateTakeOffMass()
-            => _cargoMass + _mass;
-        
-        public double CargoMass { get { return _cargoMass; } private set { _cargoMass = value; }}
-
-        private double _cargoMass;
-    }
-
-    class Airline
-    {
-        static int s_raceNumber = 0;
-
-        public Airline() {}
-
-        public void AddPlane(Plane plane)
-        {
-            _planes.Add(plane);
-            s_raceNumber++;
-        }
-        public void SortFlights()
-        {
-            _planes.Sort((lhs, rhs) => lhs.CompareTo(rhs));
-        } 
-
-        public void PrintFilgths()
-        {
-            for (int i = 0; i < 6; ++i)
-            {
-                Console.WriteLine(_planes[i]);
-            }
-        }
-
-        public int AverageTakeOffMass { get { return _averageTakeOffMass; } private set { _averageTakeOffMass = value; }}
-        private List<Plane> _planes = new List<Plane>();
-        private int _averageTakeOffMass;
-        public List<Plane> Planes { get { return _planes; } }
-    }
-
-    class Program
+    public class Program
     {
         static void Main()
         {
+            while(true)
+            {
+                Console.Clear();
+                Console.WriteLine("Variant #2. Gavrilov E.A. KE-219");
+                Console.WriteLine("1. Sort test.");
+                Console.WriteLine("2. Print test.");
+                Console.WriteLine("3. Print last test.");
+                Console.WriteLine("4. Average take off mass test.");
+                Console.WriteLine("5. Serialization test.");
+                Console.WriteLine("6. Wrong format test.");
+                Console.WriteLine("0. Exit.");
+
+                Console.WriteLine();
+                Console.WriteLine("Choose test: ");
+                Test.Type number = (Test.Type) Int32.Parse(Console.ReadLine());
+                if (number is Test.Type.Exit)
+                {
+                    break;
+                }
+                switch (number)
+                {
+                    case Test.Type.Sort:
+                        Test.SortTest();
+                        break;
+                    case Test.Type.Print:
+                        Test.PrintTest();
+                        break;
+                    case Test.Type.PrintLast:
+                        Test.PrintLastTest();
+                        break;
+                    case Test.Type.AverageTakeOffMass:
+                        Test.AverageTakeOffMassTest();
+                        break;
+                    case Test.Type.Serialization:
+                        Test.SerializationTest();
+                        break;
+                    case Test.Type.WrongFormat:
+                        Test.WrongFormatTest();
+                        break;
+                    default:
+                        break;
+                }
+            }
+            Console.Clear();
         } 
     }
 }
